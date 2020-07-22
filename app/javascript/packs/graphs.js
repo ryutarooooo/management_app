7dbc52f2f284297d22445190643596516c1627bd
ryutaro <ryutaro@icloud.com>
2020-07-22 13:57:01 +0900

    期間を指定できる様ボタン設置

--- app/javascript/packs/graphs.js
+++ app/javascript/packs/graphs.js
@@ -8,9 +8,33 @@ document.addEventListener('turbolinks:load', () => {
     const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
     const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
 
-    // データの初日・最終日
     const START_DATE = convertDate(gon.weight_records[0].date)
     const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
+
+    flatpickr.localize(flatpickr.l10ns.ja)
+
+const drawGraphForPeriod = () => {
+        let from = convertDate(document.getElementById('start-calendar').value)
+        let to = convertDate(document.getElementById('end-calendar').value)
+
+        if (from > to) {
+            alert('終了日は開始日以降の日付に設定して下さい')
+        } else {
+            drawGraph(from, to)
+        }
+    }
+
+    const periodCalendarOption = {
+    
+        disableMobile: true,
+      minDate: START_DATE,
+        maxDate: END_DATE,
+        onChange: drawGraphForPeriod
+    }
+
+  
+    const startCalendarFlatpickr = flatpickr('#start-calendar', periodCalendarOption)
+    const endCalendarFlatpickr = flatpickr('#end-calendar', periodCalendarOption)
 
     const TODAY = convertDate(new Date())
     const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
@@ -18,27 +42,24 @@ document.addEventListener('turbolinks:load', () => {
     const A_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 1, TODAY.getDate() + 1)
     const THREE_MONTHS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 3, TODAY.getDate() + 1)
 
-    // グラフを描く場所を取得
+   
     const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
 
      let chartWeight
 
-    // 期間を指定してグラフを描く
+    
     const drawGraph = (from, to) => {
-        // from から to までの期間のデータに絞る
         let records = gon.weight_records.filter((record) => {
             let date = convertDate(record.date)
             return from <= date && date <= to
         })
 
-        // 日付のみのデータを作成
+      
         let dates = records.map((record) => {
-            // 横軸のラベル表示は簡潔にしたいので，
-            // 日付 2020-01-08 を 1/8 のような形式に変換する
             return record.date.replace(/^\d+-0*(\d+)-0*(\d+)$/, '$1/$2')
         })
 
-        // 体重のみのデータを作成
+       
         let weights = records.map((record) => record.weight)
 
         let weightData = {
@@ -56,7 +77,6 @@ document.addEventListener('turbolinks:load', () => {
         let weightOption = {
             tooltips: {
                 callbacks: {
-                    // ホバー（スマホならタップ）時のラベル表示を変更
                     title: function (tooltipItems) {
                         return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
                     },
@@ -68,14 +88,12 @@ document.addEventListener('turbolinks:load', () => {
         }
 
        if (!chartWeight) {
-            // グラフが存在しないときは，作成する
             chartWeight = new Chart(chartWeightContext, {
                 type: 'line',
                 data: weightData,
                 options: weightOption
             })
         } else {
-            // グラフが存在するときは，更新する
             chartWeight.data = weightData
             chartWeight.options = weightOption
             chartWeight.update()
@@ -83,13 +101,13 @@ document.addEventListener('turbolinks:load', () => {
     }
 
     const drawGraphToToday = (from) => {
-        // データが存在する範囲に修正
         from = maxDate(from, START_DATE)
         let to = minDate(TODAY, END_DATE)
         drawGraph(from, to)
+         startCalendarFlatpickr.setDate(from)
+            endCalendarFlatpickr.setDate(to)
     }
 
-    // 過去◯週間のグラフを描くボタン
     document.getElementById('a-week-button').addEventListener('click', () => {
         drawGraphToToday(A_WEEK_AGO)
     })
@@ -105,6 +123,5 @@ document.addEventListener('turbolinks:load', () => {
     document.getElementById('three-months-button').addEventListener('click', () => {
         drawGraphToToday(THREE_MONTHS_AGO)
     })
-    // グラフの初期表示
     drawGraphToToday(A_WEEK_AGO)
 })
