0c494727df4a4ad24e5e32a25279a4225d142cf4
ryutaro <ryutaro@icloud.com>
2020-07-22 11:13:42 +0900

    体重を保存するテーブル作成

---
+++ db/migrate/20200722021131_create_graphs.rb
@@ -0,0 +1,12 @@
+class CreateGraphs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :graphs do |t|
+      t.date :date
+      t.float :weight
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :graphs, [:user_id, :date], unique: true
+  end
+end
