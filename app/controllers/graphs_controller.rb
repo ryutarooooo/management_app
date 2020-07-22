d9bc0ef4aea9307243f0b7c87b41797959b07777
ryutaro <ryutaro@icloud.com>
2020-07-22 14:08:24 +0900

    新規投稿モーダルの部分テンプレートを作成

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,11 @@
 class GraphsController < ApplicationController
   def index
     gon.weight_records = Graph.chart_data(current_user)
-    gon.weight_records = Graph.all
+  end
+
+  def create
+  end
+
+  def update
   end
 end
