b78178f728263ca12fe9bfbba366fa0e4eef5fc5
ryutaro <ryutaro@icloud.com>
2020-07-23 20:00:31 +0900

    graphsテーブルにdistanceカラムに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_021131) do
+ActiveRecord::Schema.define(version: 2020_07_23_090211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_021131) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "distance"
     t.index ["user_id", "date"], name: "index_graphs_on_user_id_and_date", unique: true
     t.index ["user_id"], name: "index_graphs_on_user_id"
   end
